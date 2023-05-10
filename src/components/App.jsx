2c982547a961ef68864a66e6c9bd7d333317f71f
Serhii Izotov <sergiyizotov@gmail.com>
2023-05-10 17:42:45 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { nanoid } from 'nanoid';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
